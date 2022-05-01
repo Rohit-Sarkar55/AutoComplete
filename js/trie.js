5ca6c57c071c3cbdc528a5c7630d1fc9b18b1782
Rohit-Sarkar55 <71070509+Rohit-Sarkar55@users.noreply.github.com>
2022-05-01 16:10:04 +0530

    Changes in trie.js

--- js/trie.js
+++ js/trie.js
@@ -1,107 +1,22 @@
-let ALPHABET_SIZE = 26;
- 
-// trie node
-class TrieNode
-{
-    constructor()
-    {
+class TrieNode{
+    constructor(ch){
+        this.ch = ch;
         this.isEnd = false;
-        this.children = new Array(ALPHABET_SIZE);
-        for (let i = 0; i < ALPHABET_SIZE; i++)
-            this.children[i] = null;
-        
-        this.words = [];
+        this.links ={};
+        this.words =[];
     }
-}
+};
 
-class Trie {
-
-    
-    constructor(){
-        this.root = new TrieNode();
+function insert(root , str , i){
+    if( i == str.length){
+        root.isEnd = true;
+        return;
     }
-
-    insert(word){
-        let len = word.length ;
-        let node = root;
-        for(let i=0; i< len; i++){
-            let index = word[i].charCodeAt(0) - 'a'.charCodeAt(0);
-            if(node.children[index] == null){
-                node.children[index] = new TrieNode();
-            }
-            node = node.children[index];
-            node.words.push(word);
-        }
-        node.isEnd = true;
+    if( ! root.links[str[i]] ){
+        root.links[str[i]] = new TrieNode(str[i]);
     }
+    root.words.push(str);
+    insert(root.links[str[i]] , str , i+1 );
+};
 
-    search(word){
-        let len = word.length ;
-        let node = root;
-        for(let i=0; i< len; i++){
-            let index = word[i].charCodeAt(0) - 'a'.charCodeAt(0);
-            if(node.children[index] == null){
-                return false;
-            }
-            node = node.children[index];
-        }
-        return node.isEnd;
-    }
-
-}
-
-let trie = new Trie();
-trie.insert("rohit");
-
-// let root;
- 
-// // If not present, inserts key into trie
-//     // If the key is prefix of trie node,
-//     // just marks leaf node
-// function insert(key)
-// {
-//     let level;
-//         let length = key.length;
-//         let index;
-       
-//         let node = root;
-       
-//         for (level = 0; level < length; level++)
-//         {
-//             index = key[level].charCodeAt(0) - 'a'.charCodeAt(0);
-//             if (node.children[index] == null)
-//                 node.children[index] = new TrieNode();
-       
-//             node = node.children[index];
-//             node.words.push(key);
-//         }
-       
-//         // mark last node as leaf
-//         node.isEnd = true;
-// }
- 
-// // Returns true if key presents in trie, else false
-// function search(key)
-// {
-//     //let level;
-//         let length = key.length;
-//         let index;
-//         let node = root;
-       
-//         for (let i = 0; i < length; i++)
-//         {
-//             index = key[i].charCodeAt(0) - 'a'.charCodeAt(0);
-       
-//             if (node.children[index] == null)
-//                 return false;
-       
-//             node = node.children[index];
-//             console.log(node.words);
-//         }
-       
-//         return (node.isEnd);
-// }
-// root = new TrieNode();
-// insert("Rohit");
-// insert("Ronaldo")
-// search("Roh");
+const root = new TrieNode();
